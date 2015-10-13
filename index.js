e230f2cffc6e309c74bab171ffa824c6b1390dbb
hdh3000 <henry.hutcheson@gmail.com>
2015-10-12 20:57:17 -0700

    added tests for key sections

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 var mocha = require('mocha');
-var chai = require('chai');
+var expect = require('chai').expect;
 var fs = require('fs');
-var nlp_compromise = require('nlp_compromise');
+var nlp = require("nlp_compromise");
 var utils = require('./helpers/utils');
+var _ = require('underscore');
+
+var resume = fs.readFileSync(__dirname + "/resumes/demoResume.txt",'utf8');
+var verbs = nlp.pos(resume).verbs();
 
 
 //read in the resume file
-var resume = fs.readFileSync(__dirname + "/resumes/demoResume.txt",'utf8');
 
-var sections = utils.parseSections(resume);
 
-console.log("-------------------------------------------",sections);  
-
+// var sections = utils.parseSections(resume);
 
 //test does it include technical skills, * projects, professional experience, education
+
+describe('resume fundementals', function () {
+
+  it('should contain the 7 key sections', function () {
+    expect(resume.match(/technical skills/i)).to.not.eql(null);
+    expect(resume.match(/strong/i)).to.not.eql(null);
+    expect(resume.match(/experienced/i)).to.not.eql(null);
+    expect(resume.match(/projects/i)).to.not.eql(null);
+    expect(resume.match(/professional/i)).to.not.eql(null);
+    expect(resume.match(/education/i)).to.not.eql(null);
+    expect(resume.match(/personal/i)).to.not.eql(null);
+  });
+
+
+});
